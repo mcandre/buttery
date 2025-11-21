e9c906b10dd0a5e49db2508d96803bee4b48726c
Andrew <githubmcandre.gumdrop164@passfwd.com>
2025-11-21 14:01:43 -0600

    update dependencies; drop dead code

--- magefile.go
+++ magefile.go
@@ -29,9 +29,6 @@ func Test() error { return mageextras.UnitTest() }
 // Deadcode runs deadcode.
 func Deadcode() error { return mageextras.Deadcode("./...") }
 
-// Gofmt runs gofmt.
-func GoFmt() error { return mageextras.GoFmt("-s", "-w") }
-
 // GoImports runs goimports.
 func GoImports() error { return mageextras.GoImports("-w") }
 
@@ -53,7 +50,6 @@ func Staticcheck() error { return mageextras.Staticcheck("./...") }
 // Lint runs the lint suite.
 func Lint() error {
 	mg.Deps(Deadcode)
-	mg.Deps(GoFmt)
 	mg.Deps(GoImports)
 	mg.Deps(GoVet)
 	mg.Deps(Errcheck)
